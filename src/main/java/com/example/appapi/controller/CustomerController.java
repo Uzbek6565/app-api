50cbe758e628a621c137c9924f84cb93c79ef0db
Bakhodir <tba-93@yandex.com>
2021-09-16 16:56:38 +0500

    Validation message is added

--- src/main/java/com/example/appapi/controller/CustomerController.java
+++ src/main/java/com/example/appapi/controller/CustomerController.java
@@ -5,10 +5,15 @@ import com.example.appapi.message.Result;
 import com.example.appapi.payload.CustomerDto;
 import com.example.appapi.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/customers")
@@ -68,5 +73,18 @@ public class CustomerController {
     @DeleteMapping("/{id}")
     public Result delete(@PathVariable Integer id){
         return customerService.delete(id);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
